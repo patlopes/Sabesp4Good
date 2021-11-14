7934604d7058e5500fce4d6b58a34e557aa3ff8b
BrunoCosta1303 <53541903+BrunoCosta1303@users.noreply.github.com>
2021-11-14 15:35:05 -0300

    FrontEnd Update
    Made the face of it look a bit better, got the modals running, still need to think the display logic for this guy.

--- screens/report.js
+++ screens/report.js
@@ -1,22 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
-export default class HomeScreen extends React.Component {
-  render() {
-    const { navigate } = this.props.navigation;
+export default function Report({ navigation }) {
+
+    const [sistema] = useState(['Selecione um sistema', 'Guarapiranga', 'Cantareira'])
+    const [sistemaSelecionado, setsistemaSelecionado] = useState([])
+
+    const [periodo] = useState(['Selecione o periodo', 'Outubro', 'Novembro'])
+    const [periodoSelecionado, setperiodoSelecionado] = useState([])
+
+    const pressHandler = () => {
+      navigation.goBack();
+    }
+    
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Hello Report</Text>
+        <Image source={require('../assets/SabespLogo.png')} style={styles.logo}/>
+        <Picker
+          selectedValue={sistemaSelecionado}
+          style={{height:30, width: 150, alignSelf:'center'}}
+          onValueChange={(itemValue) =>
+            setsistemaSelecionado(itemValue)
+          }>
+            {
+              sistema.map(cr =>{
+                return <Picker.Item label={cr} value={cr} />
+              })
+            }
+        </Picker>
+        <Picker
+          selectedValue={periodoSelecionado}
+          style={{height:30, width: 150, alignSelf:'center'}}
+          onValueChange={(itemValue) =>
+            setperiodoSelecionado(itemValue)
+          }>
+            {
+              periodo.map(cr =>{
+                return <Picker.Item label={cr} value={cr} />
+              })
+            }
+        </Picker>
       </View>
     );
-  }
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  }
+    alignItems: 'left',
+  },
+  logo: {
+    marginTop: 30,
+    marginBottom: 30,
+    width: '10%',
+    height: '10%',
+    alignSelf: 'left'
+
+  },
 });
