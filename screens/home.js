0c97f2dd0e418361c14ded40c313f0f1c3445e38
BrunoCosta1303 <53541903+BrunoCosta1303@users.noreply.github.com>
2021-11-14 15:33:55 -0300

    latest change on the home screen with the css.
    Added the logo, assets stuff, stylesheet, fixed navigation and made it pretty :)

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Image} from 'react-native';
 
 export default function Home({ navigation }) {
 
@@ -8,9 +8,37 @@ export default function Home({ navigation }) {
     }
 
     return(
-        <view>
-            <Text>Home Test</Text>
-            <Button title='Selecione seu municipio' onPress={pressHandler} />
-        </view>
+        <View style={styles.container}>
+            <Image source={require('../assets/SabespLogo.png')} style={styles.logo}/>
+            <Button color='#3382C3' title='Ir para a area de relatórios' onPress={pressHandler} />
+        </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    textStyles: {
+        marginBottom: '1%',
+        color: 'black',
+        alignSelf: 'center'
+    },
+    logo: {
+        marginTop: 60,
+        marginBottom: 60,
+        width: '30%',
+        height: '30%',
+        alignSelf: 'center'
+
+    },
+    separator: {
+        marginVertical: 4,
+        borderBottomColor: '#737373',
+        padding: 20
+    },
+
+});
+
+
